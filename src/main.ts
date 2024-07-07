1b4cf5a9196c48bbe20d5ae454807900794eb97e
Eunha1 <doquangphuc18102001@gmail.com>
2024-07-07 18:52:03 +0700

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as path from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // app.enableCors({
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   allowedHeaders: 'Content-Type,Authorization',
-  //   credentials: true,
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 204,
-  // });
-  //const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useStaticAssets(path.join(process.cwd(), ''));
   const port = process.env.PORT;
   app.use(morgan('common'));
